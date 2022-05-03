47be106495cd7e333ed7ea13a2c3b5b430297eee
Shubham Patel <shubhampatel@Shubhams-MacBook-Pro.local>
2022-05-02 22:47:34 -0400

    added simple pages blue print to run base html & other html files + added static files

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,47 @@
 """A simple flask web app"""
+import logging
 import os
+from logging.handlers import RotatingFileHandler
+
+import flask_login
 from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_wtf.csrf import CSRFProtect
+
 from app.cli import create_database
 from app.db import db
 from app.db.models import User
+from app.simple_pages import simple_pages
+from app.db import database
 
+# from flask_cors import CORS
 
-def page_not_found(e):
-    return render_template("404.html"), 404
+login_manager = flask_login.LoginManager()
+
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
 
-    app.register_error_handler(404, page_not_found)
-    db_dir = "database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+    # https://flask-login.readthedocs.io/en/latest/  <-login manager
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    # Needed for CSRF protection of form submissions and WTF Forms
+    # https://wtforms.readthedocs.io/en/3.0.x/
+    csrf = CSRFProtect(app)
+    # https://bootstrap-flask.readthedocs.io/en/stable/
+    bootstrap = Bootstrap5(app)
+
+    # these load functions with web interface
+    app.register_blueprint(simple_pages)
     # add command function to cli commands
     app.cli.add_command(create_database)
-
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
-
     return app
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    try:
+        return User.query.get(int(user_id))
+    except:
+        return None
