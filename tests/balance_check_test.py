83421418a4f969b35954dc0e5aa60cb5c90eea9e
Shubham Patel <shubhampatel@Shubhams-MacBook-Pro.local>
2022-05-09 22:11:40 -0400

    added test for balance check before and after uploading csv file

--- tests/balance_check_test.py
+++ tests/balance_check_test.py
@@ -1,8 +1,8 @@
 """ test for balance check before and after uploading csv file"""
 
 
-def test_balance(client, application):
-    """ checks csv upload"""
+def test_balance_before(client, application):
+    """ checks balance before upload"""
     with application.app_context():
         email = 'test@test.com'
         password = 'test1234'
@@ -13,6 +13,18 @@ def test_balance(client, application):
                                follow_redirects=True)
         response = client.get("/transactions")
         assert b'Current Balance: $0' in response.data
+
+
+def test_balance_after(client, application):
+    """ checks balance after upload"""
+    with application.app_context():
+        email = 'test@test.com'
+        password = 'test1234'
+        csv_test_file = 'tests/csvtest.csv'
+        response = client.post("/register", data=dict(email=email, password=password, confirm=password),
+                               follow_redirects=True)
+        response = client.post("/login", data=dict(email=email, password=password, confirm=password),
+                               follow_redirects=True)
         response = client.post("/transactions/upload", data=dict(file=open(csv_test_file, 'rb')), follow_redirects=True)
         assert response.status_code == 200
         response = client.get("/transactions")
